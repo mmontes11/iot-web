69440d5620bd89a20fd39de7176781204324f595
Martín Montes Pérez <martin.montes@nomasystems.com>
2018-09-02 13:21:59 +0200

    Added dropdown component

---
+++ src/components/dropdown.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+const Dropdown = ({ label, items, isActive, isLoading, isDisabled, onButtonClick, onItemClick }) => {
+  const dropdownClass = classNames("dropdown", { "is-active": isActive });
+  const buttonClass = classNames("button", { "is-loading": isLoading });
+  return (
+    <div className={dropdownClass}>
+      <div className="dropdown-trigger">
+        <button
+          className={buttonClass}
+          aria-haspopup="true"
+          aria-controls="dropdown-menu"
+          onClick={() => onButtonClick()}
+          disabled={isDisabled}
+        >
+          <span>{label}</span>
+          <span className="icon is-small">
+            <i className="fas fa-angle-down" aria-hidden="true" />
+          </span>
+        </button>
+      </div>
+      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {items &&
+            items.map(item => (
+              <div
+                key={btoa(item)}
+                className="dropdown-item"
+                onClick={() => onItemClick(item)}
+                onKeyPress={() => onItemClick(item)}
+                role="button"
+                tabIndex={0}
+              >
+                {item}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Dropdown.propTypes = {
+  label: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  isActive: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  isDisabled: PropTypes.bool.isRequired,
+  onButtonClick: PropTypes.func.isRequired,
+  onItemClick: PropTypes.func.isRequired,
+};
+
+export default Dropdown;
--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,6 +1,43 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { withResetOnUnmount } from "hocs/resetOnUnmount";
+import Dropdown from "components/dropdown";
 
-const Stats = () => <Fragment>{null}</Fragment>;
+const Stats = () => (
+  <div className="container is-fluid section">
+    <div className="box level">
+      <div className="level-left">
+        <div className="level-item">
+          <Dropdown
+            label="Select Type"
+            items={["Item1", "Item2", "Item3"]}
+            isActive
+            isLoading={false}
+            isDisabled={false}
+            onButtonClick={() => undefined}
+            onItemClick={() => undefined}
+          />
+          <Dropdown
+            label="Select Thing"
+            items={["Item1", "Item2", "Item3"]}
+            isActive={false}
+            isLoading
+            isDisabled={false}
+            onButtonClick={() => undefined}
+            onItemClick={() => undefined}
+          />
+          <Dropdown
+            label="Select Time Period"
+            items={["Item1", "Item2", "Item3"]}
+            isActive={false}
+            isLoading={false}
+            isDisabled
+            onButtonClick={() => undefined}
+            onItemClick={() => undefined}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default withResetOnUnmount(Stats);
