994602f80611e64b11f8ed018b5edf5822cad32f
Martín Montes Pérez <martin.montes@nomasystems.com>
2018-09-02 13:33:41 +0200

    Added columns and reset button

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -4,37 +4,47 @@ import Dropdown from "components/dropdown";
 
 const Stats = () => (
   <div className="container is-fluid section">
-    <div className="box level">
-      <div className="level-left">
-        <div className="level-item">
-          <Dropdown
-            label="Select Type"
-            items={["Item1", "Item2", "Item3"]}
-            isActive
-            isLoading={false}
-            isDisabled={false}
-            onButtonClick={() => undefined}
-            onItemClick={() => undefined}
-          />
-          <Dropdown
-            label="Select Thing"
-            items={["Item1", "Item2", "Item3"]}
-            isActive={false}
-            isLoading
-            isDisabled={false}
-            onButtonClick={() => undefined}
-            onItemClick={() => undefined}
-          />
-          <Dropdown
-            label="Select Time Period"
-            items={["Item1", "Item2", "Item3"]}
-            isActive={false}
-            isLoading={false}
-            isDisabled
-            onButtonClick={() => undefined}
-            onItemClick={() => undefined}
-          />
-        </div>
+    <div className="box columns is-half-widescreen">
+      <div className="column is-one-quarter">
+        <Dropdown
+          label="Select Type"
+          items={["Item1", "Item2", "Item3"]}
+          isActive
+          isLoading={false}
+          isDisabled={false}
+          onButtonClick={() => undefined}
+          onItemClick={() => undefined}
+        />
+      </div>
+      <div className="column is-one-quarter">
+        <Dropdown
+          label="Select Thing"
+          items={["Item1", "Item2", "Item3"]}
+          isActive={false}
+          isLoading
+          isDisabled={false}
+          onButtonClick={() => undefined}
+          onItemClick={() => undefined}
+        />
+      </div>
+      <div className="column is-one-quarter">
+        <Dropdown
+          label="Select Time Period"
+          items={["Item1", "Item2", "Item3"]}
+          isActive={false}
+          isLoading={false}
+          isDisabled
+          onButtonClick={() => undefined}
+          onItemClick={() => undefined}
+        />
+      </div>
+      <div className="column is-one-quarter">
+        <button className="button is-warning is-fullwidth" disabled>
+          <span>Reset</span>
+          <span className="icon is-small">
+            <i className="fas fa-eraser" aria-hidden="true" />
+          </span>
+        </button>
       </div>
     </div>
   </div>
