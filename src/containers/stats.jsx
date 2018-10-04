f2a34054448328d745ada885b4f98f3fec3d2f67
mmontes11 <martin11lrx@gmail.com>
2018-10-04 22:42:05 +0200

    Moved params panel state to a different container

--- src/containers/stats.jsx
+++ src/containers/stats.jsx
@@ -4,13 +4,12 @@ import { withRouter } from "react-router-dom";
 import { withResetOnUnmount } from "hocs/resetOnUnmount";
 import { compose } from "recompose";
 import PropTypes from "prop-types";
-import ParamsPanel from "components/paramsPanel";
+import ParamsPanelContainer from "containers/paramsPanelContainer";
 import FiltersPanel from "components/filtersPanel";
 import BarChart from "components/barChart";
 import Loader from "components/loader";
-import * as paramsActions from "actions/params";
-import * as commonActions from "actions/common";
 import * as statsActions from "actions/stats";
+import * as commonActions from "actions/common";
 import * as reducerHelpers from "reducers";
 
 const renderChart = (stats, isLoading) => {
@@ -22,43 +21,11 @@ const renderChart = (stats, isLoading) => {
   return null;
 };
 
-const Stats = ({
-  type,
-  observation,
-  stats,
-  isResetDisabled,
-  isLoading,
-  selectType,
-  updateType,
-  selectObservation,
-  updateObservation,
-  getStats,
-  reset,
-}) => (
+const Stats = ({ stats, isLoading, getStats, reset }) => (
   <div className="container is-fluid section">
     <div className="columns">
       <div className="column is-three-quarters">
-        <ParamsPanel
-          type={{
-            ...type,
-            label: type.selectedItem || "Select type",
-            onButtonClick: () => selectType(),
-            onItemClick: item => updateType(item),
-          }}
-          observation={{
-            ...observation,
-            label: observation.selectedItem || "Select observation",
-            onButtonClick: () => selectObservation(),
-            onItemClick: item => {
-              updateObservation(item);
-              getStats();
-            },
-          }}
-          reset={{
-            isDisabled: isResetDisabled,
-            onReset: () => reset(),
-          }}
-        />
+        <ParamsPanelContainer onParamsSelected={() => getStats()} onReset={() => reset()} />
         {renderChart(stats, isLoading)}
       </div>
       <div className="column is-one-quarter">
@@ -69,28 +36,18 @@ const Stats = ({
 );
 
 Stats.propTypes = {
-  type: PropTypes.shape({}).isRequired,
-  observation: PropTypes.shape({}).isRequired,
   stats: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  isResetDisabled: PropTypes.bool.isRequired,
   isLoading: PropTypes.bool.isRequired,
-  selectType: PropTypes.func.isRequired,
-  updateType: PropTypes.func.isRequired,
-  selectObservation: PropTypes.func.isRequired,
-  updateObservation: PropTypes.func.isRequired,
   getStats: PropTypes.func.isRequired,
   reset: PropTypes.func.isRequired,
 };
 
 const withConnect = connect(
   state => ({
-    type: state.stats.params.type,
-    observation: state.stats.params.observation,
     stats: state.stats.items,
-    isResetDisabled: state.stats.params.reset.isDisabled,
     isLoading: state.stats.isLoadingStats && reducerHelpers.isLoading(state),
   }),
-  { ...paramsActions, ...commonActions, ...statsActions },
+  { ...statsActions, ...commonActions },
 );
 
 export default compose(
