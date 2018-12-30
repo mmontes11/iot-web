7d65025b2b166955b6ae0a3c882098238475c662
Martín Montes Pérez <martin.montes@nomasystems.com>
2018-12-30 13:20:26 +0100

    Added query params for bookmarkable URLs

--- src/containers/stats.jsx
+++ src/containers/stats.jsx
@@ -3,15 +3,18 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { withResetOnUnmount } from "hocs/resetOnUnmount";
 import { compose } from "recompose";
+import QueryString from "query-string";
 import PropTypes from "prop-types";
 import StatsParamsPanel from "containers/statsParamsPanel";
 import StatsFiltersPanel from "containers/statsFiltersPanel";
 import BarChart from "components/barChart";
 import Loader from "components/loader";
 import * as statsActions from "actions/stats";
-import { updateParams } from "actions/params";
 import * as commonActions from "actions/common";
 import * as reducerHelpers from "reducers";
+import { updateParams } from "actions/params";
+import { addThingFilter } from "actions/thingFilter";
+import { addTimePeriodFilter, addCustomTimePeriodFilter } from "actions/dateFilter";
 
 const renderChart = (stats, isLoading) => {
   if (isLoading) {
@@ -30,8 +33,18 @@ class Stats extends React.Component {
       match: {
         params: { type, observation },
       },
+      location: { search },
     } = this.props;
     if (type && observation) {
+      const queryParams = QueryString.parse(search);
+      if (queryParams.thing) {
+        this.props.addThingFilter(queryParams.thing);
+      }
+      if (queryParams.timePeriod) {
+        this.props.addTimePeriodFilter(queryParams.timePeriod);
+      } else if (queryParams.startDate || queryParams.endDate) {
+        this.props.addCustomTimePeriodFilter(queryParams.startDate, queryParams.endDate);
+      }
       this.props.updateParams(type, observation);
       this.props.getStats();
     }
@@ -47,23 +60,50 @@ class Stats extends React.Component {
       this.props.history.push("/stats");
     }
   }
+  _onParamsSelected = (type, observation) => {
+    const { getStats, history, location } = this.props;
+    history.push(`/stats/${type}/${observation}${location.search}`);
+    getStats();
+  };
+  _onFiltersChange = (thing, timePeriod, startDate, endDate) => {
+    const {
+      getStats,
+      location: { pathname },
+    } = this.props;
+    const params = {};
+    if (thing) {
+      params.thing = thing;
+    }
+    if (timePeriod) {
+      params.timePeriod = timePeriod;
+    }
+    if (startDate) {
+      params.startDate = startDate.toISOString();
+    }
+    if (endDate) {
+      params.endDate = endDate.toISOString();
+    }
+    if (thing || timePeriod || startDate || endDate) {
+      const search = Object.keys(params)
+        .map(p => `${encodeURIComponent(p)}=${encodeURIComponent(params[p])}`)
+        .join("&");
+      this.props.history.push(`${pathname}?${search}`);
+    } else {
+      this.props.history.push(pathname);
+    }
+    getStats();
+  };
   render() {
-    const { stats, isLoading, getStats, reset } = this.props;
+    const { stats, isLoading, reset } = this.props;
     return (
       <div className="container is-fluid section">
         <div className="columns">
           <div className="column is-three-quarters">
-            <StatsParamsPanel 
-              onParamsSelected={(type, observation) => {
-                this.props.history.push(`/stats/${type}/${observation}`);
-                getStats()
-              }} 
-              onReset={() => reset()} 
-            />
+            <StatsParamsPanel onParamsSelected={this._onParamsSelected} onReset={() => reset()} />
             {renderChart(stats, isLoading)}
           </div>
           <div className="column is-one-quarter">
-            <StatsFiltersPanel onFiltersChange={() => getStats()} />
+            <StatsFiltersPanel onFiltersChange={this._onFiltersChange} />
           </div>
         </div>
       </div>
@@ -78,16 +118,23 @@ Stats.propTypes = {
   getStats: PropTypes.func.isRequired,
   reset: PropTypes.func.isRequired,
   updateParams: PropTypes.func.isRequired,
+  addThingFilter: PropTypes.func.isRequired,
+  addTimePeriodFilter: PropTypes.func.isRequired,
+  addCustomTimePeriodFilter: PropTypes.func.isRequired,
   match: PropTypes.shape({}).isRequired,
+  location: PropTypes.shape({}).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 const withConnect = connect(
   state => ({
     stats: state.stats.items,
     isLoading: state.stats.isLoading && reducerHelpers.isLoading(state),
-    hasError: reducerHelpers.hasError(state)
+    hasError: reducerHelpers.hasError(state),
   }),
-  { ...statsActions, ...commonActions, updateParams },
+  { ...statsActions, ...commonActions, updateParams, addThingFilter, addTimePeriodFilter, addCustomTimePeriodFilter },
 );
 
 export default compose(
